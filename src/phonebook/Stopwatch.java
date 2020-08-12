00f430170e183d663094c3022cf8184b00196e82
Nick <neverate@gmail.com>
2020-08-12 12:31:57 +0300

    stage2: stopwatch stop + elapsed

--- src/phonebook/Stopwatch.java
+++ src/phonebook/Stopwatch.java
@@ -3,15 +3,25 @@ package phonebook;
 class Stopwatch {
     private final long start;
     private Long elapsed;
+    private boolean isStopped;
 
     Stopwatch() {
         start = System.currentTimeMillis();
     }
 
     long getElapsed() {
-        if (elapsed == null) {
+        if (!isStopped) {
             elapsed = System.currentTimeMillis() - start;
         }
+        return elapsed;
+    }
+
+    long stop() {
+        if (!isStopped) {
+            isStopped = true;
+            elapsed = System.currentTimeMillis() - start;
+        }
+
         return elapsed;
     }
 }
