cb825b3807aee7d1af168d26bad7309360b38d0e
Nick <neverate@gmail.com>
2020-08-13 14:26:03 +0300

    stage3: +binary search

--- src/phonebook/search/BinarySearch.java
+++ src/phonebook/search/BinarySearch.java
@@ -5,7 +5,20 @@ import java.util.List;
 public class BinarySearch implements ISearch {
     @Override
     public boolean tryFindElement(List<String> searchIn, String searchFor) {
-        return true;
+        int start = 0;
+        int end = searchIn.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            String curEl = searchIn.get(mid);
+            if (curEl.equals(searchFor)) {
+                return true;
+            } else if (curEl.compareTo(searchFor) > 0) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
     }
 
     @Override
