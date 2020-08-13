50ade05edd93225c39cbdde996453a7bc1b92348
Nick <neverate@gmail.com>
2020-08-13 14:26:17 +0300

    stage3: +placeholder for quicksort

--- src/phonebook/sort/QuickSort.java
+++ src/phonebook/sort/QuickSort.java
@@ -2,12 +2,18 @@ package phonebook.sort;
 
 import phonebook.Stopwatch;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class QuickSort implements ISortingAlgorithm {
     @Override
     public boolean trySort(Stopwatch timer, List<String> directory) {
-        return false;
+        List<String> result = new ArrayList<>(directory);
+        result.sort(Comparator.naturalOrder());
+        Collections.copy(directory, result);
+        return true;
     }
 
     @Override
